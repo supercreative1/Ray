8fc8114323b45c6c43bf568d94b1a670a9a970ff
sasilevi <levi.sasi@gmail.com>
2025-08-03 22:25:27 +0300

    fix error

--- src/components/code-view/index.tsx
+++ src/components/code-view/index.tsx
@@ -1,5 +1,5 @@
 import Prism from "prismjs";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-tsx";
